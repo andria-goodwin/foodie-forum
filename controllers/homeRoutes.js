a5d2c57bd0252217c838c27683c168e1d30354ef
andria-goodwin <143760838+andria-goodwin@users.noreply.github.com>
2024-01-08 19:31:18 -0600

    added home routes and some css styling

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,15 @@
 const router = require('express').Router();
 
+// get route to render 'index.handlebars'
+router.get('/', (req, res) => {
+    res.render('index');
+});
+
+// get route to render 'login.handlebars'
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
 // get route to render 'post.handlebars'
 router.get('/post', (req, res) => {
     res.render('post');
