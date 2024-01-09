320b67e0927adb9a7164c4c9d3be487c2ef4c03c
Jacob Kueck <jacob.kueck@gmail.com>
2024-01-08 20:27:27 -0600

    login button and stuff

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,8 +3,8 @@ const router = require('express').Router();
 const foodRoutes = require('./foodRoutes');
 const userRoutes = require('./userRout');
 
-// router.use('/users', userRoutes);
-router.use('/food', foodRoutes);
+// router.use('/post', foodRoutes);
+router.use('/post', foodRoutes);
 router.use('/users', userRoutes);
 
 module.exports = router;
