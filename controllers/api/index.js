e70f615b9ee9f883695a2980f0d1143daea16d76
andria-goodwin <143760838+andria-goodwin@users.noreply.github.com>
2024-01-08 21:16:45 -0600

    added content to html files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,11 +3,6 @@ const router = require('express').Router();
 const foodRoutes = require('./foodRoutes');
 const userRoutes = require('./userRout');
 
-<<<<<<< HEAD
-// router.use('/post', foodRoutes);
-=======
-// router.use('/users', userRoutes);
->>>>>>> d31c8486463094f59f0d84c546e6c690f32d8163
 router.use('/post', foodRoutes);
 router.use('/users', userRoutes);
 
