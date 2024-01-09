146b7130b6afba207bf2ad86f01f1f1263fd72ac
KristoferVickers <KRISTOFERVICKERS2018@GMAIL.COM>
2024-01-08 21:10:45 -0600

    Ok Did that work?

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,49 @@
 const router = require('express').Router();
+const { Food, User } = require('../models');
+const withAuth = require('../utils/authority');
+
+// get route to render 'index.handlebars'
+router.get('/', (req, res) => {
+    res.render('index');
+});
+
+// get route to render 'login.handlebars'
+router.get('/login', (req, res) => {
+    res.render('login');
+});
 
 // get route to render 'post.handlebars'
 router.get('/post', (req, res) => {
     res.render('post');
 });
+
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+      // Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Food }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('profile', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/profile');
+      return;
+    }
+  
+    res.render('login');
+  });
 
 module.exports = router;
