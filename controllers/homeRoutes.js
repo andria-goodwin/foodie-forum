f2920067d63352689e171e6a367ca7d2f9259b57
andria-goodwin <143760838+andria-goodwin@users.noreply.github.com>
2024-01-10 11:41:36 -0600

    fixed broken link and logout button format

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -16,13 +16,6 @@ router.get('/', async (req, res) => {
 
 // get route to render 'post.handlebars'
 router.get('/post', withAuth, async (req, res) => {
-  // const foodData = await Food.findAll().catch((err) => { 
-  //   res.json(err);
-  // });
-  // // We use map() to iterate over foodData and then add .get({ plain: true }) each object to serialize it. 
-  // const foods = foodData.map((food) => food.get({ plain: true }));
-  // // We render the template, 'post', passing in foods, a new array of serialized objects.
-  // res.render('post', { foods });
 
   try {
     // Find the logged in user based on the session ID
@@ -43,34 +36,15 @@ router.get('/post', withAuth, async (req, res) => {
     res.status(500).json(err);
   }
 });
+  
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/post');
+    return;
+  }
 
-// router.get('/profile', withAuth, async (req, res) => {
-//     try {
-//       // Find the logged in user based on the session ID
-//       const userData = await User.findByPk(req.session.user_id, {
-//         attributes: { exclude: ['password'] },
-//         include: [{ model: Food }],
-//       });
-  
-//       const user = userData.get({ plain: true });
-  
-//       res.render('post', {
-//         ...user,
-//         logged_in: true
-//       });
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   });
-  
-  router.get('/login', (req, res) => {
-    // If the user is already logged in, redirect the request to another route
-    if (req.session.logged_in) {
-      res.redirect('/profile');
-      return;
-    }
-  
-    res.render('login');
-  });
+  res.render('login');
+});
 
 module.exports = router;
